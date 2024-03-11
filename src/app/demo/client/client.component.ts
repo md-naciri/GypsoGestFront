86ca5f30f17a7bb2ecc1e64561bfa0bf32c4949a
md_naciri <med.ar.naciri@gmail.com>
2024-03-11 14:37:49 +0000

    fix client component ts

--- src/app/demo/client/client.component.ts
+++ src/app/demo/client/client.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ClientInterface } from './types/client-interface';
+import { ClientService } from './client.service';
 import { CardComponent } from "../../theme/shared/components/card/card.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-client',
-    standalone: true,
-    templateUrl: './client.component.html',
-    styleUrl: './client.component.scss',
-    imports: [CommonModule, CardComponent]
+  selector: 'app-client',
+  standalone: true,
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.scss'],
+  imports: [CommonModule,CardComponent]
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  clients: ClientInterface[] = [];
 
+  constructor(public dialog: MatDialog, private clientService: ClientService) { }
+
+  ngOnInit(): void {
+    this.getClients();
+  }
+
+  getClients(): void {
+    this.clientService.getAllClients().subscribe(
+      (data) => {
+        console.log(data);
+        this.clients = data;
+      },
+      (error) => {
+        console.log(error.error.error);
+      }
+    );
+  }
 }
