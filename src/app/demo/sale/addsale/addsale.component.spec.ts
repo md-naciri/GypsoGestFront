0f34354a8254d1e3f39885429e21a461d3acdde0
md_naciri <med.ar.naciri@gmail.com>
2024-03-12 14:06:11 +0000

    create sale and addsale components

---
+++ src/app/demo/sale/addsale/addsale.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddsaleComponent } from './addsale.component';
+
+describe('AddsaleComponent', () => {
+  let component: AddsaleComponent;
+  let fixture: ComponentFixture<AddsaleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddsaleComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AddsaleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/demo/sale/addsale/addsale.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addsale',
+  standalone: true,
+  imports: [],
+  templateUrl: './addsale.component.html',
+  styleUrl: './addsale.component.scss'
+})
+export class AddsaleComponent {
+
+}
