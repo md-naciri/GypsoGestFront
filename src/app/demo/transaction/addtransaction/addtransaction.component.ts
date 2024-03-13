3d9e5dd0f2b9ccf79225c405e4ee9c342c1dd53b
md_naciri <med.ar.naciri@gmail.com>
2024-03-13 00:39:12 +0000

    generate addtransaction component

---
+++ src/app/demo/transaction/addtransaction/addtransaction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addtransaction',
+  standalone: true,
+  imports: [],
+  templateUrl: './addtransaction.component.html',
+  styleUrl: './addtransaction.component.scss'
+})
+export class AddtransactionComponent {
+
+}
