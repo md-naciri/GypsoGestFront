090137ff4cb91a136f06b06941fb6af91a8ea892
md_naciri <med.ar.naciri@gmail.com>
2024-03-13 11:53:34 +0000

    PaymentReversalService - done

--- src/app/demo/payment-reversal/payment-reversal.service.ts
+++ src/app/demo/payment-reversal/payment-reversal.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { PaymentReversalInterface } from './types/payment-reversal-interface';
+import { PaymentReversalRequestInterface } from './types/payment-reversal-request-interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentReversalService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllPaymentReversals(): Observable<PaymentReversalInterface[]> {
+    return this.http.get<PaymentReversalInterface[]>(environment.apiURL + 'returneds');
+  }
+
+  createPaymentReversal(reversalData: PaymentReversalRequestInterface): Observable<PaymentReversalInterface> {
+    return this.http.post<PaymentReversalInterface>(environment.apiURL + 'returneds', reversalData);
+  }
 }
