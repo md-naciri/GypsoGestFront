ff1630edeeffee4e13a7e14fc263b36b73f89a2a
md_naciri <med.ar.naciri@gmail.com>
2024-03-13 18:27:53 +0000

    client-sold.component.html - done

--- src/app/demo/accounting/types/sold-interface.ts
+++ src/app/demo/accounting/types/sold-interface.ts
@@ -1,10 +1,10 @@
 export interface SoldInterface {
     client: {
         id: number;
-        firstName: string;
-        lastName: string;
-        email: string;
-        cin: string;
+        FirstName: string;
+        LastName: string;
+        Email: string;
+        CIN: string;
     };
     total: number;
 }
