519d502c32aa40c2b9767446c0c384b9fa978159
md_naciri <med.ar.naciri@gmail.com>
2024-03-13 01:01:43 +0000

    TransactionComponent class - done

--- src/app/demo/transaction/transaction.component.ts
+++ src/app/demo/transaction/transaction.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransactionInterface } from './types/transaction-interface';
+import { TransactionService } from './transaction.service';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from 'src/app/theme/shared/components/card/card.component';
+
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.scss'
 })
 export class TransactionComponent {
+  transactions: TransactionInterface[] = [];
 
+  constructor(private transactionService: TransactionService) { }
+
+  ngOnInit(): void {
+    this.getTransactions();
+  }
+
+  getTransactions(): void {
+    this.transactionService.getAllTransactions().subscribe(
+      (response: any) => {
+        if (response && response.data) {
+          this.transactions = response.data;
+          console.log(this.transactions);
+          
+      } else {
+        console.log("Invalid transaction response format");  
+      }},
+      (error) => {
+        console.log(error.error.error);
+      }
+    );
+  }
 }
