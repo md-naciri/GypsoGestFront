bd8bf7bf939ad8b5d6d88679868e4f98f3b15cf8
md_naciri <med.ar.naciri@gmail.com>
2024-03-13 18:18:53 +0000

    ClientSoldComponent - done

--- src/app/demo/accounting/client-sold/client-sold.component.ts
+++ src/app/demo/accounting/client-sold/client-sold.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { SoldInterface } from '../types/sold-interface';
+import { AccountingService } from '../accounting.service';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from 'src/app/theme/shared/components/card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client-sold',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent, FormsModule],
   templateUrl: './client-sold.component.html',
   styleUrl: './client-sold.component.scss'
 })
-export class ClientSoldComponent {
 
+export class ClientSoldComponent implements OnInit {
+  @Input() clientId: number | null = null; // Input to receive specific client ID
+  soldData: SoldInterface | null = null;
+
+  constructor(private accountingService: AccountingService) { }
+
+  ngOnInit(): void {
+    this.getClientSold();
+  }
+
+  getClientSold(): void {
+    this.accountingService.getClientSold(this.clientId).subscribe(
+      (response: any) => {
+        if (response && response.data) {
+          this.soldData = response.data;
+          console.log(this.soldData);
+          
+      } else {
+        console.log("Invalid sold response format");  
+      }},
+      (error) => {
+        console.log(error.error.error);
+      }
+    );
+  }
 }
