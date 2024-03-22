bfada4a6d8e8db9f4b176178ea145c5a45fbe208
md_naciri <med.ar.naciri@gmail.com>
2024-03-22 10:31:45 +0000

    Dockerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true,
+  production: false,
+  apiURL: 'http://localhost:8080/api/v1/',
+  apiUrlAuth:'http://localhost:8080/api/v1/auth/'
 };
+
