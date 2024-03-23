819fd4d9c4bb01f31125aedbadeb064d426b689b
md_naciri <med.ar.naciri@gmail.com>
2024-03-23 14:17:46 +0000

    add deleteUser method in UserComponent

--- src/app/demo/user/user.component.ts
+++ src/app/demo/user/user.component.ts
@@ -4,6 +4,7 @@ import { CardComponent } from "../../theme/shared/components/card/card.component
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../pages/authentication/auth.service';
 import { AppUserInterface } from './types/appUser.interface';
+import { ConfirmDialogComponent } from '../confirmdialog/confirm.dialog.component';
 
 
 @Component({
@@ -15,6 +16,8 @@ import { AppUserInterface } from './types/appUser.interface';
 })
 export class UserComponent {
   users : AppUserInterface[] = [];
+  errorMessage: string = '';
+  successMessage: string = '';
 
   constructor(public dialog: MatDialog,private authService:AuthService) {
   }
@@ -31,6 +34,38 @@ export class UserComponent {
         console.log(error.error.error);
       }
     );
+  }
+
+  deleteUser(username: string): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        message: 'Are you sure you want to delete this client?' // Confirmation message
+      }
+    });
+  
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.authService.deleteUser(username).subscribe(
+          () => {
+            // Remove the deleted client from the user array
+            this.users = this.users.filter(client => client.username !== username);
+            console.log(`Employee with username ${username} deleted successfully.`);
+            this.successMessage = "Employee has been deleted successfully";
+          },
+          (error) => {
+            this.errorMessage = error.error.error;
+          }
+        );
+      }
+    });
+  }
+
+  closeSuccessAlert(): void {
+    this.successMessage = '';
+  }
+
+  closeErrorAlert(): void {
+    this.successMessage = '';
   }
   
 }
